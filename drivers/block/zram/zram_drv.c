95d7e73d949b4f46e9bb2bd010f17826ae7bca21
kingbri <bdashore3@gmail.com>
2019-12-02 13:01:34 -0500

    zram: Expose compression algorithms to userspace
    This makes it easier to debug/edit zram compression algorithms for the user.
    The user can change from the default algorithm to something else
    
    File in sysfs: "/sys/module/zram/parameters/default_compressor"
    
    Signed-off-by: kingbri <bdashore3@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -34,6 +34,7 @@
 #include <linux/sysfs.h>
 #include <linux/debugfs.h>
 #include <linux/cpuhotplug.h>
+#include <linux/moduleparam.h>
 
 #include "zram_drv.h"
 
@@ -42,7 +43,9 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
-static const char *default_compressor = CONFIG_ZRAM_DEF_COMP;
+static char *default_compressor = CONFIG_ZRAM_DEF_COMP;
+
+module_param(default_compressor, charp, 0644);
 
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
