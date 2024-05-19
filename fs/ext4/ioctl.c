cce9d49083a5d0d1e73ba15a19519682777273e1
Daffa Prasetya <kumaraprastya@gmail.com>
2024-05-19 19:59:56 +0000

    Revert "ioctl.c : fs/ext4 & vlan_core.c : net/8021q " from this commit 8bbf3ced1ef7c92daa4da8a7ebb45d0586438ca0
    Signed-off : Daffa06<kumaraprastya@gmail.com>

--- fs/ext4/ioctl.c
+++ fs/ext4/ioctl.c
@@ -538,9 +538,7 @@ static int ext4_shutdown(struct super_block *sb, unsigned long arg)
 
 	switch (flags) {
 	case EXT4_GOING_FLAGS_DEFAULT:
-		ret = freeze_bdev(sb->s_bdev);
-		if (ret)
-			return ret;
+		freeze_bdev(sb->s_bdev);
 		set_bit(EXT4_FLAGS_SHUTDOWN, &sbi->s_ext4_flags);
 		thaw_bdev(sb->s_bdev, sb);
 		break;
