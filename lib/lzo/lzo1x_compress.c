569a8cb596339eea477534ac894304e23e9a9f73
Elyani <meuthiaelyani@proton.me>
2024-05-22 17:12:32 +0000

    Revert "UPSTREAM: lib/lzo: implement run-length encoding"
    This reverts commit d2bc76f0e0723d0b47c74b47365badfda377366c.

--- lib/lzo/lzo1x_compress.c
+++ lib/lzo/lzo1x_compress.c
@@ -20,8 +20,7 @@
 static noinline size_t
 lzo1x_1_do_compress(const unsigned char *in, size_t in_len,
 		    unsigned char *out, size_t *out_len,
-		    size_t ti, void *wrkmem, signed char *state_offset,
-		    const unsigned char bitstream_version)
+		    size_t ti, void *wrkmem)
 {
 	const unsigned char *ip;
 	unsigned char *op;
@@ -36,10 +35,9 @@ lzo1x_1_do_compress(const unsigned char *in, size_t in_len,
 	ip += ti < 4 ? 4 - ti : 0;
 
 	for (;;) {
-		const unsigned char *m_pos = NULL;
+		const unsigned char *m_pos;
 		size_t t, m_len, m_off;
 		u32 dv;
-		u32 run_length = 0;
 literal:
 		ip += 1 + ((ip - ii) >> 5);
 next:
@@ -47,7 +45,7 @@ next:
 			break;
 		dv = get_unaligned_le32(ip);
 
-		if (dv == 0 && bitstream_version) {
+		if (dv == 0) {
 			const unsigned char *ir = ip + 4;
 			const unsigned char *limit = ip_end
 				< (ip + MAX_ZERO_RUN_LENGTH + 1)
@@ -82,19 +80,17 @@ next:
 					ALIGN((uintptr_t)ir, 4)) &&
 					(ir < limit) && (*ir == 0))
 				ir++;
-			if (IS_ALIGNED((uintptr_t)ir, 4)) {
-				for (; (ir + 4) <= limit; ir += 4) {
-					dv = *((u32 *)ir);
-					if (dv) {
+			for (; (ir + 4) <= limit; ir += 4) {
+				dv = *((u32 *)ir);
+				if (dv) {
 #  if defined(__LITTLE_ENDIAN)
-						ir += __builtin_ctz(dv) >> 3;
+					ir += __builtin_ctz(dv) >> 3;
 #  elif defined(__BIG_ENDIAN)
-						ir += __builtin_clz(dv) >> 3;
+					ir += __builtin_clz(dv) >> 3;
 #  else
 #    error "missing endian definition"
 #  endif
-						break;
-					}
+					break;
 				}
 			}
 #endif
@@ -116,7 +112,7 @@ next:
 		t = ip - ii;
 		if (t != 0) {
 			if (t <= 3) {
-				op[*state_offset] |= t;
+				op[-2] |= t;
 				COPY4(op, ii);
 				op += t;
 			} else if (t <= 16) {
@@ -147,17 +143,6 @@ next:
 					*op++ = *ii++;
 				} while (--t > 0);
 			}
-		}
-
-		if (unlikely(run_length)) {
-			ip += run_length;
-			run_length -= MIN_ZERO_RUN_LENGTH;
-			put_unaligned_le32((run_length << 21) | 0xfffc18
-					   | (run_length & 0x7), op);
-			op += 4;
-			run_length = 0;
-			*state_offset = -3;
-			goto finished_writing_instruction;
 		}
 
 		m_len = 4;
@@ -242,6 +227,7 @@ m_len_done:
 
 		m_off = ip - m_pos;
 		ip += m_len;
+		ii = ip;
 		if (m_len <= M2_MAX_LEN && m_off <= M2_MAX_OFFSET) {
 			m_off -= 1;
 			*op++ = (((m_len - 1) << 5) | ((m_off & 7) << 2));
@@ -291,9 +277,6 @@ m_len_done:
 			*op++ = (m_off << 2);
 			*op++ = (m_off >> 6);
 		}
-		*state_offset = -2;
-finished_writing_instruction:
-		ii = ip;
 		goto next;
 	}
 	*out_len = op - out;
@@ -310,19 +293,11 @@ int lzogeneric1x_1_compress(const unsigned char *in, size_t in_len,
 	size_t l = in_len;
 	size_t t = 0;
 	signed char state_offset = -2;
-	unsigned int m4_max_offset;
 
-	// LZO v0 will never write 17 as first byte (except for zero-length
-	// input), so this is used to version the bitstream
-	if (bitstream_version > 0) {
-		*op++ = 17;
-		*op++ = bitstream_version;
-		m4_max_offset = M4_MAX_OFFSET_V1;
-	} else {
-		m4_max_offset = M4_MAX_OFFSET_V0;
-	}
-
-	data_start = op;
+	// LZO v0 will never write 17 as first byte,
+	// so this is used to version the bitstream
+	*op++ = 17;
+	*op++ = LZO_VERSION;
 
 	while (l > 20) {
 		size_t ll = l <= (m4_max_offset + 1) ? l : (m4_max_offset + 1);
@@ -331,8 +306,8 @@ int lzogeneric1x_1_compress(const unsigned char *in, size_t in_len,
 			break;
 		BUILD_BUG_ON(D_SIZE * sizeof(lzo_dict_t) > LZO1X_1_MEM_COMPRESS);
 		memset(wrkmem, 0, D_SIZE * sizeof(lzo_dict_t));
-		t = lzo1x_1_do_compress(ip, ll, op, out_len, t, wrkmem,
-					&state_offset, bitstream_version);
+		t = lzo1x_1_do_compress(ip, ll, op, out_len,
+					t, wrkmem, &state_offset);
 		ip += ll;
 		op += *out_len;
 		l  -= ll;
@@ -345,7 +320,7 @@ int lzogeneric1x_1_compress(const unsigned char *in, size_t in_len,
 		if (op == data_start && t <= 238) {
 			*op++ = (17 + t);
 		} else if (t <= 3) {
-			op[state_offset] |= t;
+			op[-2] |= t;
 		} else if (t <= 18) {
 			*op++ = (t - 3);
 		} else {
