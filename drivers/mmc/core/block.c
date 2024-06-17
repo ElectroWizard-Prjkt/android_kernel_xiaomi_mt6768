8ccfa42f576b84814bf043faec316fe136e3c2b0
Daffa06 <kumaraprastya@gmail.com>
2024-06-18 03:11:59 +0700

    Adjust code from upstream 345 5b45b9995fdd49bc036ddc42b3853ceac00fd2be for core/block.c
    Signed-off: Daffa06<Daffa06@github.com>

--- drivers/mmc/core/block.c
+++ drivers/mmc/core/block.c
@@ -44,6 +44,7 @@
 #include <linux/mmc/host.h>
 #include <linux/mmc/mmc.h>
 #include <linux/mmc/sd.h>
+#include <linux/mmc/block.h>
 
 #ifdef CONFIG_MMC_FFU
 #include <linux/mmc/ffu.h>
@@ -1287,39 +1288,36 @@ static const struct block_device_operations mmc_bdops = {
 };
 
 static int mmc_blk_part_switch_pre(struct mmc_card *card,
-				   unsigned int part_type)
+                   unsigned int part_type)
 {
-	const unsigned int mask = EXT_CSD_PART_CONFIG_ACC_MASK;
-	const unsigned int rpmb = EXT_CSD_PART_CONFIG_ACC_RPMB;
-	int ret = 0;
+    int ret = 0;
 
-	#if ((part_type & mask) == rpmb) {
-		if (card->ext_csd.cmdq_en) {
-			ret = mmc_cmdq_disable(card);
-			if (ret)
-				return ret;
-		}
-		mmc_retune_pause(card->host);
-	}
+    #if ((part_type & mask) == rpmb) {
+        if (card->ext_csd.cmdq_en) {
+            ret = mmc_cmdq_disable(card);
+            if (ret)
+                return ret;
+        }
+        mmc_retune_pause(card->host);
+    }
 #endif
-	return ret;
+
+    return ret;
 }
 
 static int mmc_blk_part_switch_post(struct mmc_card *card,
-				    unsigned int part_type)
+                    unsigned int part_type)
 {
-	const unsigned int mask = EXT_CSD_PART_CONFIG_ACC_MASK;
-	const unsigned int rpmb = EXT_CSD_PART_CONFIG_ACC_RPMB;
-	int ret = 0;
+    int ret = 0;
 
-	#if ((part_type & mask) == rpmb) {
-		mmc_retune_unpause(card->host);
-		if (card->reenable_cmdq && !card->ext_csd.cmdq_en)
-			ret = mmc_cmdq_enable(card);
-	}
+    #if ((part_type & mask) == rpmb) {
+        mmc_retune_unpause(card->host);
+        if (card->reenable_cmdq && !card->ext_csd.cmdq_en)
+            ret = mmc_cmdq_enable(card);
+    }
 #endif
 
-	return ret;
+    return ret;
 }
 
 static inline int mmc_blk_part_switch(struct mmc_card *card,
